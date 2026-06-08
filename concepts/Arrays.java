0de914fa6aea237969380376f698b873ad0f2ba0
Hrishikesh Shinde <shrishikesh925@gmail.com>
2026-06-08 14:21:30 +0000

    revised the arrays starting again from a week of break

--- concepts/Arrays.java
+++ concepts/Arrays.java
@@ -4,36 +4,44 @@ public class Arrays
 {
     public static void main(String[] args)
     {
-        Scanner value = new Scanner(System.in);
-        System.out.println("Ok so lets start with 1d array do lets add value in array 1 with the size of 10:");
-        int[] array = new int[5];
-        for (int i = 0; i < 5; i++)
+        Scanner newarray = new Scanner(System.in);
+        System.out.println("This file takes in and prints array elements");
+        System.out.print("Please enter the size of array you want within (20 ints): ");
+        int size = newarray.nextInt();
+        int[] array = new int[size];
+        System.out.println("Ok so the size of the array is " + size);
+        System.out.println("So now enter " + size + " int elements");
+        for(int i = 0; i < size; i++)
         {
-            array[i] = value.nextInt();
+            System.out.print("Enter the " + (i + 1) + " th element please: ");
+            array[i] = newarray.nextInt();
         }
-        System.out.println("So your array is : ");
-        for (int i = 0; i < 5; i++)
+        System.out.println("Damn that is a good array you got there, here it is your 1 d array: ");
+        for(int i = 0; i < size; i++)
         {
             System.out.print(array[i] + ", ");
         }
-        System.out.println("\nOk so now lets do the same with 2D array because why not yeah? and built a matrix");
+        System.out.println("\nHaha, that was fun, Now let's make a 2 d array");
+        System.out.println("lets keep the size of this 2d array simple alright? 3 rows and 3 elements this time.");
         int[][] array2 = new int[3][3];
-        
-        for (int i = 0; i < 3; i++)
+        System.out.println("Ok so since this is a 3 X 3 array let's enter 9 elements alright?");
+        for(int i = 0; i < 3; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for(int j = 0; j < 3; j++)
             {
-                array2[i][j] = value.nextInt();
+                System.out.print("Enter element [" + (i + 1) + "][" + (j + 1) + "]: ");
+                array2[i][j] = newarray.nextInt();
             }
         }
-        for (int i = 0; i < 3; i++)
+        System.out.println("Damn that was a good 2d array, lets print but this time in matrix form");
+        for(int i = 0; i < 3; i++)
         {
-            for (int j = 0; j < 3; j++)
+            for(int j = 0; j < 3; j++)
             {
                 System.out.print(array2[i][j] + " ");
             }
-            System.out.print("\n");
+            System.out.println();
         }
-        value.close();
+        newarray.close();
     }
 }
